caf3d01ebe0d9c6bd026513e5fb40eea120313e5
jonkurtis <jonathankurtis@gmail.com>
2018-06-14 21:09:36 -0400

    Front-page styling

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php get_sidebar( 'footerfull' ); ?>
 
-<div class="wrapper" id="wrapper-footer">
+<div class="wrapper bg-primary" id="wrapper-footer">
 
 	<div class="<?php echo esc_attr( $container ); ?>">
 
@@ -25,18 +25,14 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-							<a href="<?php  echo esc_url( __( 'http://wordpress.org/','understrap' ) ); ?>"><?php printf( 
+							<?php printf( 
 							/* translators:*/
-							esc_html__( 'Proudly powered by %s', 'understrap' ),'WordPress' ); ?></a>
+							esc_html__( 'Copyright &copy; %s %s LLC', 'understrap' ),  date("Y"), get_bloginfo('name') ); ?>
 								<span class="sep"> | </span>
 					
 							<?php printf( // WPCS: XSS ok.
 							/* translators:*/
-								esc_html__( 'Theme: %1$s by %2$s.', 'understrap' ), $the_theme->get( 'Name' ),  '<a href="'.esc_url( __('http://understrap.com', 'understrap')).'">understrap.com</a>' ); ?> 
-				
-							(<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Version: %1$s', 'understrap' ), $the_theme->get( 'Version' ) ); ?>)
+								esc_html__( 'Website designed and managed by %s.', 'understrap' ), '<a href="'.esc_url( __('https://fullmetamarketing.com', 'fullmetamarketing')).'">FullMetaMarketing.com</a>' ); ?> 
 					</div><!-- .site-info -->
 
 				</footer><!-- #colophon -->
