fe42e7af592fbaf99339949a065ef6d1ffda9ca0
Jonathan <jonathankurtis@gmail.com>
2018-06-20 22:18:24 -0400

    services section styling

--- section-templates/our_services-loop.php
+++ section-templates/our_services-loop.php
@@ -8,7 +8,7 @@ $args = array(
 // Custom query.
 $query = new WP_Query( $args );
 
-function get_all_them_cpt_posts_products($post_type){
+function get_all_them_cpt_posts_services($post_type){
     //$post_type = 'our_services';
     $count_posts = wp_count_posts( $post_type );
 
@@ -25,10 +25,10 @@ if ( $query->have_posts() ) {
         $query->the_post();
  
         // Contents of the queried post results go here.
-        ?> <div class="col-lg-<?php echo (12 / get_all_them_cpt_posts_products( 'our_services' ));?> col-md-6 col-sm-12 text-center">
+        ?> <div class="col-lg-<?php echo (12 / get_all_them_cpt_posts_services( 'our_services' ));?> col-md-6 col-sm-12 text-center">
                 <div class="services-wrapper">
-                    <div> <i class="fa <?php echo get_field( 'fa_icon' ); ?> fa-5x" aria-hidden="true"></i> </div>
-                    <h4> <?php the_title(); ?> </h4>
+                    <div class="service-icon"> <i class="fa <?php echo get_field( 'fa_icon' ); ?> fa-4x" aria-hidden="true"></i> </div>
+                    <h5> <?php the_title(); ?> </h4>
                     <p> <?php the_content(); ?> </p>
                 </div>
             </div> <?php
