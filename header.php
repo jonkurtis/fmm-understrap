5352369c3efa60dfaa5978ea72f8dbb32bd77e33
Jonathan <jonathankurtis@gmail.com>
2018-07-17 22:02:09 -0400

    switch to light header for logo.

--- header.php
+++ header.php
@@ -28,11 +28,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="hfeed site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar" class="sticky-top" itemscope itemtype="http://schema.org/WebSite">
+	<div id="wrapper-navbar" class="sticky-top bg-light" itemscope itemtype="http://schema.org/WebSite">
 
 		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+		<nav class="navbar navbar-expand-md navbar-light bg-light">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container" >
