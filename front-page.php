75b8aa62795bf753311d46d5d33f98e8d4fb1cf0
jonkurtis <jonathankurtis@gmail.com>
2018-09-10 23:39:51 -0400

    initial commit on laragon

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
+ * This is only a test.
  * @package understrap
  */
 
