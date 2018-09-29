55c5a4cefc090f7359f1c6a910a57fd65ddadb1b
jonkurtis <jonathankurtis@gmail.com>
2018-09-29 11:06:42 -0400

    updated product card formatting for consistent styling.

--- section-templates/products-loop.php
+++ section-templates/products-loop.php
@@ -25,13 +25,20 @@ if ( $query->have_posts() ) {
         $query->the_post();
  
         // Contents of the queried post results go here.
-        ?> <div class="col-lg-<?php echo (12 / get_all_them_cpt_posts_products( 'products' ));?> col-md-6 col-sm-12 text-center">
-                <div class="card shadow">
+        ?> <div class="col-lg-<?php echo (12 / get_all_them_cpt_posts_products( 'products' ));?> col-md-6 col-sm-12 text-center mb-4">
+                <div class="card shadow d-flex h-100">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4> <?php the_title(); ?></h4>
+                        </div>
+                    </div>
                     <div class="card-img-top"> <?php the_post_thumbnail(); ?> </div>
-                    <div class="card-body">
-                        <div class="card-title"><h4> <?php the_title(); ?></h4></div>
-                        <p class="card-text"> <?php echo get_field('product_summary'); ?> </p>
-                        <a class="btn btn-outline-primary btn-lg" href="<?php the_permalink(); ?>">Learn More</a>     
+                    
+                    <div class="card-body ">
+                        <p class="card-text"> <?php echo get_field('product_summary'); ?> </p>                          
+                    </div>
+                    <div class="card-footer">
+                            <a class="btn btn-outline-primary btn-lg" href="<?php the_permalink(); ?>">Learn More</a>
                     </div>
                 </div>
             </div> <?php
