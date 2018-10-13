7dab6753a9dfc22f438773ba31b4dd2617a3547d
jonkurtis <jonathankurtis@gmail.com>
2018-10-13 10:43:42 -0400

    Add Hero Heading and buttons to hero slider and fixed hero slider navigation functionality.

--- front-page.php
+++ front-page.php
@@ -23,12 +23,22 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 
 <div class="wrapper" id="index-wrapper">
-
 	<div id="hero-slider" class="carousel slide" data-ride="carousel" data-interval="5000">
-		<ol class="carousel-indicators">
-			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		<div class="position-absolute d-flex h-100 align-items-center w-100" style="z-index: 4;">
+			<div class="container py-5 text-center hero-content text-white">
+				<h2 class="display-4 hero-heading">Eco Construction Group</h2>
+				<p class="hero-subheading"> Central Florida's premiere custom builder of energy efficient and eco-friendly construction.</p>
+				<br>
+				<div class="hero-buttons">
+					<a class="btn btn-primary btn-lg" href="/gallery">View Gallery</a>
+					<a class="btn btn-success btn-lg" href="#" target="_blank" role="button">Get A Free Quote</a>
+				</div>
+			</div>
+		</div>
+		<ol class="carousel-indicators" style="z-index: 5;">
+			<li data-target="#hero-slider" data-slide-to="0" class="active"></li>
+			<li data-target="#hero-slider" data-slide-to="1"></li>
+			<li data-target="#hero-slider" data-slide-to="2"></li>
 		</ol>
 		<div class="carousel-inner">
 			<div class="carousel-item active">
@@ -44,15 +54,16 @@ $container   = get_theme_mod( 'understrap_container_type' );
 			<img class="d-block w-100" src="/wp-content/uploads/2018/09/hero-1.jpg" alt="Third slide">
 			</div>
 		</div>
-		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		<a class="carousel-control-prev" href="#hero-slider" role="button" data-slide="prev" style="z-index:6">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
 		</a>
-		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		<a class="carousel-control-next" href="#hero-slider" role="button" data-slide="next" style="z-index:6">
 			<span class="carousel-control-next-icon" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
 		</a>
 	</div>
+	
 
 	<section id="main-callout" class="bg-success">
 		<div class="container">
