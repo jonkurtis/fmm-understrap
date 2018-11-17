64fc8a697467cf16ef9005e2026870fc9043bd3c
jonkurtis <jonathankurtis@gmail.com>
2018-11-17 14:37:38 -0500

    Added transparent navbar, solid on scroll only on home page script.

--- js/theme.js
+++ js/theme.js
@@ -3965,3 +3965,17 @@ function scrollTo (element) {
         top: element.offsetTop
     });
 };
+
+var nav = document.querySelector('#wrapper-navbar'); // Identify target
+
+if (this.window.location.pathname == "/"){
+    window.addEventListener('scroll', function(event) { // To listen for event
+        event.preventDefault();
+            if (window.scrollY >= 200) { // Just an example
+                nav.classList.add('solid-nav'); // or default color
+            } else {
+                nav.classList.remove('solid-nav');
+            }});
+} else {
+    nav.classList.add('solid-nav');
+};
