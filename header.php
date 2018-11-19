d84031df6810553a6837feb0a8d35dd20ae89784
jonkurtis <jonathankurtis@gmail.com>
2018-11-18 19:57:56 -0500

    changed nav-link color, size and transform to uppercase.

--- header.php
+++ header.php
@@ -20,6 +20,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 	<?php wp_head(); ?>
 </head>
 
